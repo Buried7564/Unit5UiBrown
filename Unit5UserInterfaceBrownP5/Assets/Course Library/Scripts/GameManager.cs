4860eb9d823003591805a1c4f19091335772be57
Kayden Brown <145067189+Buried7564@users.noreply.github.com>
2024-05-10 13:51:15 -0700

    5/10/24

--- Unit5UserInterfaceBrownP5/Assets/Course Library/Scripts/GameManager.cs
+++ Unit5UserInterfaceBrownP5/Assets/Course Library/Scripts/GameManager.cs
@@ -1,25 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using TMPro;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
+    private int score;
+    public TextMeshProUGUI scoreText;
     public List<GameObject> targets;
     private float spawnRate = 1.0f;
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
+    public Button restartButton;
     // Start is called before the first frame update
     void Start()
     {
+        isGameActive = true;
+        score = 0;
+        UpdateScore(0);
         StartCoroutine(SpawnTarget());
     }
-
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
         }
+        
+    }
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+        restartButton.gameObject.SetActive(true);
     }
 }
--- Unit5UserInterfaceBrownP5/Assets/Course Library/Scripts/Target.cs
+++ Unit5UserInterfaceBrownP5/Assets/Course Library/Scripts/Target.cs
@@ -1,19 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
+    private GameManager gameManager;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
+    public TextMeshProUGUI gameOverText;
 
     // Start is called before the first frame update
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
-        targetRb.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
+        targetRb.AddForce(Vector3.up * Random.Range(12, 19), ForceMode.Impulse);
         targetRb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10), 
         Random.Range(-10, 10), ForceMode.Impulse);
         transform.position = new Vector3(Random.Range(-4, 4), - 6);
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -23,10 +29,23 @@ public class Target : MonoBehaviour
     }
     private void OnMouseDown()
     {
-        Destroy(gameObject);
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            gameManager.UpdateScore(pointValue);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        }
+    }
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
     }
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad")) 
+        { 
+            gameManager.GameOver(); 
+        }
     }
 }
